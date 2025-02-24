385571707efb7626fcc5662ad48d457b1abb94b6
Denise Nanni <denise.nanni1@gmail.com>
2025-02-24 10:26:34 +0100

    test initially unlocked

--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -1,12 +1,16 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void isInitiallyUnlocked() {
+        SmartDoorLock doorLock = new SmartDoorLockImpl();
+        assertFalse(doorLock.isLocked());
     }
+
 }
